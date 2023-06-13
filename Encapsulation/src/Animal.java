d928584b6a807bbd8ca760fcb5d43c6546d79dc6
Antonis <107072477+vlantonakos@users.noreply.github.com>
2023-06-13 11:38:38 +0300

    Add files via upload

---
+++ Encapsulation/src/Animal.java
@@ -0,0 +1,29 @@
+
+
+public class Animal {
+
+	private String name;
+	private double averageWeight;
+	private int numberOfLegs;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getAverageWeight() {
+		return averageWeight;
+	}
+	public void setAverageWeight(double averageWeight) {
+		this.averageWeight = averageWeight;
+	}
+	public int getNumberOfLegs() {
+		return numberOfLegs;
+	}
+	public void setNumberOfLegs(int numberOfLegs) {
+		this.numberOfLegs = numberOfLegs;
+	}
+	
+	
+}
